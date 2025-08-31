f1d3fb982c848434584337527f04bc9c42cb5fbc
Domirando <vohidjonovnamaftuna@gmail.com>
2025-08-31 19:59:39 +0500

    feat: transient window added

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,158 @@
-use gtk::prelude::*;
-use relm4::adw::prelude::*;
-use relm4::prelude::*;
-struct AppModel {
-    counter: u8,
-    expander: adw::ExpanderRow
+// Don't show relm4::gtk 4.10 deprecations.
+// We can't replace them without raising the relm4::gtk requirement to 4.10.
+#![allow(deprecated)]
+
+use std::convert::identity;
+
+use relm4::gtk::glib;
+use relm4::gtk::prelude::{ButtonExt, GtkWindowExt, WidgetExt};
+use relm4::MessageBroker;
+use relm4::{
+    Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmApp,
+    SimpleComponent,
+};
+
+static DIALOG_BROKER: MessageBroker<DialogMsg> = MessageBroker::new();
+
+struct Dialog {
+    visible: bool,
 }
 
 #[derive(Debug)]
-enum AppMsg {
-    Increment,
-    Decrement,
+enum DialogMsg {
+    Show,
+    Hide,
 }
 
 #[relm4::component]
-impl SimpleComponent for AppModel {
-    type Init = u8;
-    type Input = AppMsg;
-    type Output = ();
+impl SimpleComponent for Dialog {
+    type Init = ();
+    type Input = DialogMsg;
+    type Output = ButtonMsg;
 
     view! {
-        gtk::Window {
-            set_title: Some("Simple app"),
-            set_default_size: (300, 100),
+        dialog = relm4::gtk::Dialog {
+            #[watch]
+            set_visible: model.visible,
+            set_modal: true,
 
-            gtk::Box {
-                set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
-                set_vexpand: true,
-                set_hexpand: true,
+            #[wrap(Some)]
+            set_child = &relm4::gtk::Label {
+                set_width_request: 200,
+                set_height_request: 200,
+                set_halign: relm4::gtk::Align::Center,
+                set_valign: relm4::gtk::Align::Center,
+                #[watch]
+                set_label: "Sozlamalar"
+            },
 
-                gtk::Button {
-                    set_label: "Increment",
-                    connect_clicked => AppMsg::Increment,
-                },
-
-                gtk::Button {
-                    set_label: "Decrement",
-                    connect_clicked => AppMsg::Decrement,
-                },
-
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("Counter: {}", model.counter),
-                    set_margin_all: 5,
-                },
-
-                gtk::Box{
-                    adw::PreferencesGroup {
-                        set_title: "Extensions",
-
-                        #[name(expander)]
-                        add = &adw::ExpanderRow {
-                            set_title: "AppIndicator and KStatusNotifierItem Support",
-                            set_subtitle: "appindicatorsupport@rgcjonas.gmail.com",
-                        }
-                    }
-
-
-                }
-
-
-
+            connect_close_request[sender] => move |_| {
+                sender.input(DialogMsg::Hide);
+                glib::Propagation::Stop
             }
         }
     }
 
-    // Initialize the component.
     fn init(
-        counter: Self::Init,
+        _init: Self::Init,
         root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let mut model = AppModel { 
-            counter,
-            expander: adw::ExpanderRow::new()
-        };
-        // Insert the code generation of the view! macro here
+        let model = Dialog { visible: false };
         let widgets = view_output!();
-        model.expander = widgets.expander.clone();
-        let justify = gtk::Justification::Left;
-        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 1);
-        vbox.set_align(gtk::Align::Start);
-        
-        for i in 1..5 {
-            // if i == 1{ 
-            // }
-            let label = &gtk::Label::builder().label("sadasdasd").justify(justify).build();
-            vbox.append(label);
-            model.expander.add_row(&vbox);         
-        }
         ComponentParts { model, widgets }
     }
 
     fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
         match msg {
-            AppMsg::Increment => {
-                self.counter = self.counter.wrapping_add(1);
-            }
-            AppMsg::Decrement => {
-                self.counter = self.counter.wrapping_sub(1);
-            }
+            DialogMsg::Show => self.visible = true,
+            DialogMsg::Hide => self.visible = false,
         }
     }
 }
 
+struct Button {
+    #[allow(dead_code)]
+    dialog: Controller<Dialog>,
+}
+
+#[derive(Debug)]
+enum ButtonMsg {}
+
+#[relm4::component]
+impl SimpleComponent for Button {
+    type Init = ();
+    type Input = ButtonMsg;
+    type Output = AppMsg;
+
+    view! {
+        button = &relm4::gtk::Button {
+            set_label: "Show the dialog",
+            connect_clicked => move |_| {
+                DIALOG_BROKER.send(DialogMsg::Show);
+            }
+        }
+    }
+
+    fn init(
+        _init: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        // We don't have access to the parent window from here
+        // but we can just use the button to set the transient window for the dialog.
+        // Relm4 will get the window later by calling [`WidgetExt::root()`]
+        // on the button once all widgets are connected.
+        let dialog = Dialog::builder()
+            //.transient_for(&root)
+            .launch_with_broker((), &DIALOG_BROKER)
+            .forward(sender.input_sender(), identity);
+
+        let model = Button { dialog };
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, _msg: Self::Input, _sender: ComponentSender<Self>) {}
+}
+
+#[derive(Debug)]
+enum AppMsg {}
+
+struct App {
+    button: Controller<Button>,
+}
+
+#[relm4::component]
+impl SimpleComponent for App {
+    type Init = ();
+    type Input = AppMsg;
+    type Output = ();
+
+    view! {
+        main_window = relm4::gtk::ApplicationWindow {
+            set_default_size: (500, 250),
+            set_child: Some(model.button.widget()),
+        }
+    }
+
+    fn init(
+        _init: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let button = Button::builder()
+            .launch(())
+            .forward(sender.input_sender(), identity);
+        let model = App { button };
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, _msg: Self::Input, _sender: ComponentSender<Self>) {}
+}
+
 fn main() {
-    let app = RelmApp::new("relm4.example.simple");
-    app.run::<AppModel>(0);
+    let app = RelmApp::new("relm4.example.transient_dialog");
+    app.run::<App>(());
 }
--- src/playground.rs
+++ src/playground.rs
@@ -1,7 +1,97 @@
-use gtk::prelude::*;
+use relm4::adw::prelude::*;
 use relm4::prelude::*;
 
+struct AppModel {
+    clicked: bool,
+    expander: adw::ExpanderRow
+}
+
+#[derive(Debug)]
+enum AppMsg {
+    Settings,
+}
+
+#[relm4::component]
+impl SimpleComponent for AppModel {
+    type Init = bool;
+    type Input = AppMsg;
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            set_title: Some("Simple app"),
+            set_default_size: (300, 100),
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 20,
+                set_margin_all: 20,
+                set_vexpand: true,
+                set_hexpand: true,
+
+                gtk::Box{
+
+                    adw::PreferencesGroup {
+                        #[name(expander)]
+                        add = &adw::ExpanderRow {
+                            set_title: "AppIndicator and KStatusNotifierItem Support",
+                            set_subtitle: "appindicatorsupport@rgcjonas.gmail.com",
+                        }
+                    }
+                },
+               gtk::Button {
+                    set_label: "Sozlamalar",
+                    connect_clicked => AppMsg::Settings,
+               },
+
+                gtk::Label {
+                    #[watch]
+                    set_label: &format!("Bosildi: {}", model.clicked),
+                    set_margin_all: 5,
+                },
+            }
+        }
+    }
+
+    // Initialize the component.
+    fn init(
+        clicked: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let mut model = AppModel { 
+            clicked,
+            expander: adw::ExpanderRow::new()
+        };
+        // Insert the code generation of the view! macro here
+        let widgets = view_output!();
+        model.expander = widgets.expander.clone();
+        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 1);
+        vbox.set_align(gtk::Align::Start);
+        
+//         for i in 1..5 {
+//             // if i == 1{
+//             // }
+//             let label = &gtk::Label::builder().label("sadasdasd").justify(justify).build();
+//             vbox.append(label);
+//             model.expander.add_row(&vbox);
+//         }
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            AppMsg::Settings => {
+                self.clicked = !self.clicked;
+            }
+        }
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let app = RelmApp::new("relm4.example.simple");
+    app.run::<AppModel>(false);
 }
+
+
+
